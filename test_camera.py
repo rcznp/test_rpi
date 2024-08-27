da7c6e2a77a9ab557d3c0821cca4493d8676ca1e
rayzheng59 <rayzheng59@gmail.com>
2024-08-28 01:42:54 +0800

    changed name now really got change Pi to PiCamera

--- test_camera.py
+++ test_camera.py
@@ -1,7 +1,7 @@
-from picamera import Pi 
+from picamera import PiCamera
 from time import sleep
 #test change
-camera = Picamera()
+camera = PiCamera()
 camera.start_preview()
 sleep(5)
 camera.capture('/home/pi/Desktop/image.jpg')
